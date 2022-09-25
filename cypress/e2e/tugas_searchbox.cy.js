e2c47639dd2b1245547b8a74533636ee75556b61
adesaputraa <adesaputra0928@gmail.com>
2022-09-25 10:54:32 +0700

    tugas searchboc

---
+++ cypress/e2e/tugas_searchbox.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+describe('Searchbox Test', function () {
+    before(() => {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+    })
+
+    it('Should type into searchbox and submit', () => {
+        cy.get('#searchTerm').type('online {enter}')
+    });
+
+    it('Should show search result page', () => {
+        cy.get('h2').should('contain.text', 'Search Results:')
+        cy.get('a').contains('Zero - Free Access to Online Banking').click()
+        cy.url().should('include', '/online-banking.html')
+
+        cy.get('#searchTerm').type('online {enter}')
+        cy.get('a').contains('Zero - Online Statements').click()
+        cy.get('h3').should('have.text', 'Log in to ZeroBank')
+    });
+
+    // it('Should show search result page', () => {
+    //     cy.get('h2').should('contain.text', 'Search Results:')
+    //     cy.get('#searchTerm').type('online {enter}')
+    //     cy.get('a').contains('Zero - Online Statements').click()
+    //     cy.get('h3').should('have.text', 'Log in to ZeroBank')
+    // });
+
+
+
+});
